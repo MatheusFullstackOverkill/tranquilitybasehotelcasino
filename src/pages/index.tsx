39c89e61c6041c15111748ed2f956ef716e392d6
Matheus <matheudbaf0123@gmail.com>
2023-01-12 03:41:10 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,35 @@ import { Page } from '../components/Page'
 export default function Home() {
   return (
     <Page className='home'>
-      <div className='section intro'>
+      <div className='section banner'>
         <h1 className='tranquility-font'>TRANQUILITY BASE<br/> HOTEL & CASINO</h1>
       </div>
-      <div className='section content py-5'>
-        <h1 className='tranquility-font d-block mb-3'>Take it easy for a little while...</h1>
-        <p>
-          Advertise in imaginative ways
-          Start your free trial today
-          Come on in, the water&apos;s lovely
-          Look, you could meet someone you like
-          During the meteor strike
-          It is that easy
-          Lunar surface on a Saturday night
-          Dressed up in silver and white
-          With colored old grey whistle test lights
-        </p>
+      <div className='section intro content d-flex flex-column align-items-center py-5'>
+        <h1 className='d-block mb-4'>Take it easy for a little while...</h1>
+        <div className='w-100 d-flex flex-row'>
+          <div className='img-container'>
+            <img src='assets/images/pexels-pixabay-53464.jpg'/>
+          </div>
+          <div className='text-container'>
+            <p>
+              Advertise in imaginative ways <br/>
+              Start your free trial today <br/>
+              Come on in, the water&apos;s lovely <br/>
+              Look, you could meet someone you like <br/>
+              During the meteor strike <br/>
+              It is that easy <br/>
+              Lunar surface on a Saturday night <br/>
+              Dressed up in silver and white <br/>
+              With colored old grey whistle test lights
+            </p>
+          </div>
+        </div>
       </div>
-      <div className='section rooms'></div>
+      <div className='section rooms'>
+        <div className='img-container corridor-bg'>
+          <img src='assets/images/corridor.jpg'/>
+        </div>
+      </div>
       <div className='section casino'></div>
       <div className='section entertainment'></div>
       <footer></footer>
